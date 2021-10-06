674217cfadc551e0c5ade7b969995c8f02e7f874
Johnson Ngandjui <ngandjui.johnson@gmail.com>
2021-10-06 08:38:02 -0400

    missing data

--- src/preprocessing/missingdata.py
+++ src/preprocessing/missingdata.py
@@ -3,6 +3,8 @@ import pandas as pd
 data= pd.read_csv("D:\Senior\Capstone\data-science-enviroment\data\BIG FIVE 1995-2019.csv")
 
 missing = data.isna().sum()
+size = data.sizek=
 
-print(missing)
+print(size)
+print("checking for missing Data \n",missing)
 #0 missing data, this is great!
