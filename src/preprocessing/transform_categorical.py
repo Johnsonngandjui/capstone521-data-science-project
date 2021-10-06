8b022b51469c0b24d6851b6966c9e1fddf6c18d5
Johnson Ngandjui <ngandjui.johnson@gmail.com>
2021-10-06 12:41:55 -0400

    todo, seperate unique teams according to league

--- src/preprocessing/transform_categorical.py
+++ src/preprocessing/transform_categorical.py
@@ -3,19 +3,22 @@ import pandas as pd
 data= data = pd.read_csv('D:\Senior\Capstone\data-science-enviroment\data\BIG FIVE 1995-2019.csv')
 data.dtypes
 
+data.columns = data.columns.str.replace(' ','_')
+
 #redundent information, FT/HT Team 1 or 2 to get the following information
 data= data.drop(columns=['FT', 'HT'])
 for col in data.columns:
     print(col)
 
 #transforming countries from categorical to numerical
-unique_values = data.Country.unique()
+unique_values_country = data.Country.unique()
 X = data.iloc[:,:].values
 
 
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 labelencoder_X = LabelEncoder()
 X[:,5] = labelencoder_X.fit_transform(X[:,5])
 Z = pd.DataFrame(X)
 
 #Z.to_csv('D:/Senior/Capstone/data-science-enviroment/data/Preprocessed_V1.csv')
+unique_values = data.Team_1.unique()
