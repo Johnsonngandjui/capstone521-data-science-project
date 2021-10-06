0d6186ba7a2b4d3b9bcd86797d01c343e149fd6e
Johnson Ngandjui <ngandjui.johnson@gmail.com>
2021-10-06 12:14:10 -0400

    changed counrty to numerical

--- src/preprocessing/missingdata.py
+++ src/preprocessing/missingdata.py
@@ -3,7 +3,7 @@ import pandas as pd
 data= pd.read_csv("D:\Senior\Capstone\data-science-enviroment\data\BIG FIVE 1995-2019.csv")
 
 missing = data.isna().sum()
-size = data.sizek=
+size = data.size
 
 print(size)
 print("checking for missing Data \n",missing)
